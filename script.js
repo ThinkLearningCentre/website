128435c8dda38e9ec0e4ffaa77ea45cea36a4399
George Soteriou <george.soteriou.1997@gmail.com>
2019-01-04 10:03:05 +0200

    added google analytics

--- script.js
+++ script.js
@@ -11,6 +11,14 @@ $(document).ready(function(){
 	    $(".post").slideDown(500);
 	   	$(".footer").delay(500).slideDown(500);
  	}
+	
+	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+	ga('create', 'UA-129266614-1', 'auto');
+	ga('send', 'pageview');
 });
 
 $(window).load(function() {
